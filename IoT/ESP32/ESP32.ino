3b30d6476432fa33eb564e8651fe73e4920066db
Thomas <tienhau.it@gmail.com>
2025-05-23 11:27:49 +0700

    Update - Khắc phục lỗi khởi tạo góc không đúng và động cơ Servo quay mượt hơn

--- IoT/ESP32/ESP32.ino
+++ IoT/ESP32/ESP32.ino
@@ -6,6 +6,8 @@ RobotArm robot_arm;
 
 void setup() {
   Serial.begin(115200);
+  delay(1000);
+  Serial.println("Attaching servos...");
 
   /* Connect Wifi 
     connectWiFi();
@@ -23,7 +25,7 @@ void setup() {
   */
 
   robot_arm.attachAll();
-  robot_arm.centerAll();
+  robot_arm.centerAllSmooth();
   Serial.println("Robot arm ready. Send command via Serial.");
 
   /* Init MQTT
@@ -60,33 +62,33 @@ void loop() {
     }
   */
 
-    robot_arm.updateAll();
+  robot_arm.updateAll();
 
-    if (Serial.available()) {
-      String inputCommand = Serial.readStringUntil('\n');
-      inputCommand.trim();
+  if (Serial.available()) {
+    String inputCommand = Serial.readStringUntil('\n');
+    inputCommand.trim();
 
-      int spaceIndex = inputCommand.indexOf(' ');
-      if (spaceIndex == -1) {
-        Serial.println("Invalid command format. Use: <servoX> <angle>");
-        return;
-      }
-
-      Serial.print("Command: ");
-      Serial.println(inputCommand);
-
-      String joint = inputCommand.substring(0, spaceIndex);
-      int angle = inputCommand.substring(spaceIndex + 1).toInt();
-
-      if (joint == "servo1") robot_arm.servo1.moveTo(angle);
-      else if (joint == "servo2") robot_arm.servo2.moveTo(angle);
-      else if (joint == "servo3") robot_arm.servo3.moveTo(angle);
-      else if (joint == "servo4") robot_arm.servo4.moveTo(angle);
-      else if (joint == "servo5") robot_arm.servo5.moveTo(angle);
-      else if (joint == "servo6") robot_arm.servo6.moveTo(angle);
-      else if (joint == "debug") robot_arm.debugAll();
-      else Serial.println("Unknown command");
+    int spaceIndex = inputCommand.indexOf(' ');
+    if (spaceIndex == -1) {
+      Serial.println("Invalid command format. Use: <servoX> <angle>");
+      return;
     }
+
+    Serial.print("Command: ");
+    Serial.println(inputCommand);
+
+    String joint = inputCommand.substring(0, spaceIndex);
+    int angle = inputCommand.substring(spaceIndex + 1).toInt();
+
+    if (joint == "servo1") robot_arm.servo1.moveTo(angle);
+    else if (joint == "servo2") robot_arm.servo2.moveTo(angle);
+    else if (joint == "servo3") robot_arm.servo3.moveTo(angle);
+    else if (joint == "servo4") robot_arm.servo4.moveTo(angle);
+    else if (joint == "servo5") robot_arm.servo5.moveTo(angle);
+    else if (joint == "servo6") robot_arm.servo6.moveTo(angle);
+    else if (joint == "debug") robot_arm.debugAll();
+    else Serial.println("Unknown command");
+  }
 
   /* Trying connect MQTT
     static unsigned long lastReconnectAttempt = 0;
--- IoT/ESP32/RoboticArm.h
+++ IoT/ESP32/RoboticArm.h
@@ -5,137 +5,156 @@
 #include <ESP32Servo.h>
 
 class Joint {
-  private:
-    Servo servo;
-    int pin;
-    int minAngle;
-    int maxAngle;
-    int currentAngle;
+private:
+  Servo servo;
+  int pin;
+  int minAngle;
+  int maxAngle;
+  int currentAngle;
+  int defaultAngle;
 
-    // Biến cho chuyển động mượt không dùng delay
-    bool isMoving = false;
-    int targetAngle = 90;
-    unsigned long lastMoveTime = 0;
-    int moveStep = 1;
-    unsigned long moveInterval = 5; // ms
+  // Biến cho chuyển động mượt không dùng delay
+  bool isMoving = false;
+  int targetAngle = 90;
+  unsigned long lastMoveTime = 0;
+  int moveStep = 1;
+  unsigned long moveInterval = 3;  // ms
 
-  public:
-    Joint(int pin, int minA = 0, int maxA = 180, int defaultA = 90) {
-      this->pin = pin;
-      this->minAngle = minA;
-      this->maxAngle = maxA;
-      this->currentAngle = defaultA;
+public:
+  Joint(int pin, int minA = 0, int maxA = 180, int defaultA = 90) {
+    this->pin = pin;
+    this->minAngle = minA;
+    this->maxAngle = maxA;
+    this->defaultAngle = defaultA;
+    this->currentAngle = defaultA;
+  }
+
+  void moveTo(int angle) {
+    angle = constrain(angle, minAngle, maxAngle);
+    if (angle == currentAngle) return;
+
+    targetAngle = angle;
+    moveStep = (targetAngle > currentAngle) ? 1 : -1;
+    isMoving = true;
+    lastMoveTime = millis();
+  }
+
+  void attach() {
+    servo.setPeriodHertz(50);  // Tần số servo thường là 50Hz
+    servo.attach(pin);
+    servo.write(currentAngle);  // Đặt ban đầu
+
+    Serial.print("Servo at pin ");
+    Serial.print(pin);
+    Serial.print(" starting at angle ");
+    Serial.println(currentAngle);
+    Serial.println("--------------------------------");
+  }
+
+  void center() {
+    moveTo(defaultAngle);
+  }
+
+  void debugSweep(int delayTime = 15) {
+    for (int angle = minAngle; angle <= maxAngle; angle++) {
+      servo.write(angle);
+      delay(delayTime);
     }
-
-    void moveTo(int angle) {
-      angle = constrain(angle, minAngle, maxAngle);
-      if (angle == currentAngle) return;
-
-      targetAngle = angle;
-      moveStep = (targetAngle > currentAngle) ? 1 : -1;
-      isMoving = true;
-      lastMoveTime = millis();
+    for (int angle = maxAngle; angle >= minAngle; angle--) {
+      servo.write(angle);
+      delay(delayTime);
     }
+    currentAngle = (minAngle + maxAngle) / 2;
+    servo.write(currentAngle);
+  }
 
-    void attach() {
-      servo.setPeriodHertz(50);  // Tần số servo thường là 50Hz
-      servo.attach(pin);
-      servo.write(currentAngle); // Đặt ban đầu
-    }
+  void update() {
+    if (!isMoving) return;
 
-    void center() {
-      moveTo((minAngle + maxAngle) / 2);
-    }
-
-    void debugSweep(int delayTime = 15) {
-      for (int angle = minAngle; angle <= maxAngle; angle++) {
-        servo.write(angle);
-        delay(delayTime);
-      }
-      for (int angle = maxAngle; angle >= minAngle; angle--) {
-        servo.write(angle);
-        delay(delayTime);
-      }
-      currentAngle = (minAngle + maxAngle) / 2;
+    if (millis() - lastMoveTime >= moveInterval) {
+      currentAngle += moveStep;
       servo.write(currentAngle);
-    }
+      lastMoveTime = millis();
 
-    void update() {
-      if (!isMoving) return;
-
-      if (millis() - lastMoveTime >= moveInterval) {
-        currentAngle += moveStep;
-        servo.write(currentAngle);
-        lastMoveTime = millis();
-
-        if (currentAngle == targetAngle) {
-          isMoving = false;
-        }
+      if (currentAngle == targetAngle) {
+        isMoving = false;
       }
     }
+  }
 
-    int getCurrentAngle() {
-      return currentAngle;
-    }
+  int getCurrentAngle() {
+    return currentAngle;
+  }
+
+  bool isServoMoving() {
+    return isMoving;
+  }
 };
 
 class RobotArm {
-  public:
-    Joint servo1;
-    Joint servo2;
-    Joint servo3;
-    Joint servo4;
-    Joint servo5;
-    Joint servo6;
+public:
+  Joint servo1;
+  Joint servo2;
+  Joint servo3;
+  Joint servo4;
+  Joint servo5;
+  Joint servo6;
 
-    RobotArm():
-      servo1(4, 0, 180, 30),
-      servo2(16, 0, 180, 90),
+  RobotArm()
+    : servo1(4, 0, 180, 30),
+      servo2(16, 0, 180, 80),
       servo3(17, 0, 180, 30),
-      servo4(5, 0, 180, 100),
+      servo4(5, 0, 160, 120),
       servo5(18, 0, 180, 80),
       servo6(19, 0, 180, 85) {}
 
-    void attachAll() {
-      servo6.attach();
-      delay(300);
-      servo5.attach();
-      delay(300);
-      servo4.attach();
-      delay(300);
-      servo3.attach();
-      delay(300);
-      servo2.attach();
-      delay(300);
-      servo1.attach();
-    }
+  void attachAll() {
+    servo6.attach();
+    delay(500);
+    servo5.attach();
+    delay(500);
+    servo4.attach();
+    delay(500);
+    servo4.attach();
+    delay(500);
+    servo3.attach();
+    delay(500);
+    servo2.attach();
+    delay(500);
+    servo1.attach();
+  }
 
-    void centerAll() {
-      servo1.center();
-      servo2.center();
-      servo3.center();
-      servo4.center();
-      servo5.center();
-      servo6.center();
-    }
+  void centerAllSmooth() {
+    Joint* joints[] = { &servo6, &servo5, &servo4, &servo3, &servo2, &servo1 };
+    const int numJoints = sizeof(joints) / sizeof(joints[0]);
 
-    void debugAll() {
-      servo1.debugSweep();
-      servo2.debugSweep();
-      servo3.debugSweep();
-      servo4.debugSweep();
-      servo5.debugSweep();
-      servo6.debugSweep();
-    }
+    for (int i = 0; i < numJoints; ++i) {
+      joints[i]->center();
 
-    void updateAll() {
-      servo1.update();
-      servo2.update();
-      servo3.update();
-      servo4.update();
-      servo5.update();
-      servo6.update();
+      while (joints[i]->isServoMoving()) {
+        joints[i]->update();
+        delay(5);
+      }
     }
+  }
+
+  void debugAll() {
+    servo1.debugSweep();
+    servo2.debugSweep();
+    servo3.debugSweep();
+    servo4.debugSweep();
+    servo5.debugSweep();
+    servo6.debugSweep();
+  }
+
+  void updateAll() {
+    servo1.update();
+    servo2.update();
+    servo3.update();
+    servo4.update();
+    servo5.update();
+    servo6.update();
+  }
 };
 
 #endif
