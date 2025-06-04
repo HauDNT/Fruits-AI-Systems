60b5c7dcb28edc6c19d956002d515de0a229f485
Thomas <tienhau.it@gmail.com>
2025-06-04 19:54:24 +0700

    Fix - Sửa lỗi xung đột DTO và Form Data khi gửi cấu hình Raspberry mới

--- server/src/modules/raspberry/dto/raspberry-config.dto.ts
+++ server/src/modules/raspberry/dto/raspberry-config.dto.ts
@@ -1,16 +1,18 @@
-import {IsArray, IsNotEmpty, IsNumber, IsString,} from "class-validator";
+import { Type, Transform } from 'class-transformer';
+import {IsArray, IsNotEmpty, IsNumber, IsString, ArrayNotEmpty, IsInt} from "class-validator";
 import {FruitTypeIdsDto} from "@/modules/raspberry/dto/fruit-type-ids.dto";
 
 export class RaspberryConfigDto {
     @IsNumber()
     @IsNotEmpty({message: 'Mã thiết bị không được bỏ trống'})
+    @Type(() => Number)
     device_id: number;
 
     @IsString()
     @IsNotEmpty({message: 'Mã định danh thiết bị không được bỏ trống'})
     device_code: string;
 
-    @IsArray()
-    @IsNotEmpty({message: 'Danh sách nhãn học máy phân loại không được bỏ trống'})
+    @IsNotEmpty({message: 'Các nhãn được chọn phải nằm trong một mảng'})
+    @Type(() => FruitTypeIdsDto, { keepDiscriminatorProperty: true })
     labels: FruitTypeIdsDto[];
 }
